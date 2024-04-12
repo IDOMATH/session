7edf6c8718d8ab101946cd9fa5a4bf96508ec554
IDOMATH <btdothemath@gmail.com>
2024-04-11 22:20:56 -0500

    Adding insert to memory store

--- session.go
+++ session.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"sync"
 	"time"
 )
-
-type Session struct {
-	Store  Store
-	Id     string
-	Values map[interface{}]interface{}
-}
 
 type Store interface {
 	Insert(token string, b []byte, expiresAt time.Time) (err error)
 	Get(token string) (b []byte, exists bool, err error)
 	Delete(token string) (err error)
+}
+
+type item struct {
+	obj       []byte
+	expiresAt int64
+}
+
+type MemoryStore struct {
+	items map[string]item
+	mu    sync.RWMutex
+}
+
+func (s *MemoryStore) Insert(token string, b []byte, expiresAt time.Time) error {
+	s.mu.Lock()
+	s.items[token] = item{
+		obj:       b,
+		expiresAt: expiresAt.UnixNano(),
+	}
+	s.mu.Unlock()
+
+	return nil
 }
