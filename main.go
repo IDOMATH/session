6f0d7a41c079e7082f25a817d94a59d3405e9acb
IDOMATH <btdothemath@gmail.com>
2024-04-14 22:29:56 -0500

    Getting main set up for testing

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	router := http.NewServeMux()
+
+	router.HandleFunc("GET /", handleHome)
+}
+
+func handleHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Welcome Home"))
 }
