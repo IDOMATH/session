2054073e1ced2256208badb9ca7be5968c2cc844
IDOMATH <btdothemath@gmail.com>
2024-06-21 16:32:31 -0500

    cleaning up session

--- store.go
+++
@@ -1,9 +0,0 @@
-package session
-
-import "time"
-
-type Store interface {
-	Insert(token string, b []byte, expiresAt time.Time) (err error)
-	Get(token string) (b []byte, exists bool, err error)
-	Delete(token string) (err error)
-}
